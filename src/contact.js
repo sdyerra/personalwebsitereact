0f9b0495ed1a197d3cce661a870a7115a22f004b
sdyerra <saideepak.yerra@gmail.com>
2023-08-23 20:40:27 -0400

    changed intro

--- src/contact.js
+++ src/contact.js
@@ -15,11 +15,7 @@ function Contact() {
     return (
         <div className="Contact-header">
             <img className='contactpic' src={Contactpic} alt="Grad Pic" />
-            <p className='intro'>Hi! I'm a recent graduate of Virginia Tech Computer Science.
-                I have experience programming in Java, Python, C, Kotlin, and React JS; I would also love to work with new languages.
-                I am currently looking for programming and cybersecurity roles.
-                Outside of programming skills, I have experience with network and server administration,
-                advanced hardware and software troubleshooting, and penetration testing.</p>
+            <p className='intro'>Hi there! I'm a recent graduate of Virginia Tech with a degree in Computer Science. I have experience programming in languages such as Java, Python, C, Kotlin, and React JS. Beyond programming, my skill set includes network and server administration, advanced hardware/software troubleshooting, and penetration testing. When I'm not immersed in the world of computers, you can find me working on my car or spending quality time with my dogs.</p>
             <Container className='icons-container'>
                 <Row>
                     <Popup position='top' trigger={ <Col xs="auto"><img onClick={() => {navigator.clipboard.writeText("saideepak.yerra@gmail.com")}} src={emaillogo} className='icons' alt="email"/></Col>}> <a>Email copied to clipboard.</a> </Popup> 
